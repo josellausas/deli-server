5037fcb54d7a939efb2dcf22e49b7da44637d564
zunware <jose@josellausas.com>
2019-10-04 16:46:36 -0500

    Basic server logger middleware

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const express = require('express');
+const moment = require('moment');
 const path = require('path');
 const currentDir = __dirname;
+
+const members = require('./src/Members');
+
+// Create some middleware that intercepts all requests
+const logger = (req, res, next) => {
+  const now = moment().format();
+  const { protocol } = req;
+  const host = req.get('host');
+  const page = req.originalUrl
+  const requestedUrl = `${protocol}://${host}${page}`;
+  console.log(`[${now}] ${requestedUrl}`);
+  //------------------------------------------------
+  //------------- BOTTOM OF FUNC -------------------
+  next(); 
+  //--------- DO NOT REMOVE. required!!! -----------
+}
+
 // Constants
 const PORT = process.env.PORT || 5000;
 const app = express();
-// Set static dir
+// Middleware
 app.use(express.static(path.join(currentDir, 'public')))
+app.use(logger)
 
-// HOME
+// API
 app.get('/', (req, res) => {
   // res.send('Hello World');
   // res.json() JSON !
   // res.render() templates
   res.sendFile(path.join(currentDir, 'public', 'index.html'));
+});
+
+app.get('/api/members', (req, res) => {
+  return res.json(members);
 });
 
 app.listen(
---
+++ src/Members.js
@@ -0,0 +1,6 @@
+const members = [{
+    "name": "Bob Testerson",
+    "email": "bob_testerson@gmail.com",
+  },
+];
+module.exports = members;
